701db0cb493f2d23be6b3a717adf4d36d100a1cb
Hameed-Ur-Rehman25 <hameedkhan944873@gmail.com>
2025-02-10 16:49:55 +0500

    Back to normal Stage no error

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:buyer_centric_app_v2/routes/app_routes.dart';
-import 'package:buyer_centric_app_v2/theme/colors.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:buyer_centric_app_v2/utils/powered_by.dart';
